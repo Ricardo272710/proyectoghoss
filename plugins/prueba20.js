fc6b719290ed9c30c51b24999f31c282d59a490e
YallicoPe <yhairmusica950@gmail.com>
2024-07-01 01:05:30 -0500

    Update prueba20.js

--- plugins/prueba20.js
+++ plugins/prueba20.js
@@ -1,19 +1,55 @@
-let handler = async (m, {conn, command}) => {
-  let url = Purgatorio[Math.floor(Math.random() * Purgatorio.length)];
-    await conn.sendFile( 
-     m.chat, 
-     url, 
-     "gimage.jpge", 
-     ` 
- *Purgatorio*`.trim(), m)
-};
-handler.help = ["Purgatorio"];
-handler.tags = ["internet"];
-handler.command = /^(Purgatorio)$/i;
-export default handler;
+let handler = async (m, { isPrems, conn }) => {
+let time = global.db.data.users[m.sender].lastcofre + 0 // 36000000 10 Horas //86400000 24 Horas
+if (new Date - global.db.data.users[m.sender].lastcofre < 0) throw `[❗𝐈𝐍𝐅𝐎❗] 𝚈𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚂𝚃𝙴 𝚃𝚄 𝙲𝙾𝙵𝚁𝙴\𝚗𝚅𝚄𝙴𝙻𝚅𝙴 𝙴𝙽 *${msToTime(time - new Date())}* 𝙿𝙰𝚁𝙰 𝚅𝙾𝙻𝚅𝙴𝚁 𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚁`
 
-global.Purgatorio = [
-  "https://telegra.ph/file/09edd89c06631a6671658.jpg`",
-];
+let img = 'https://telegra.ph/file/09edd89c06631a6671658.jpg'
+let dia = Math.floor(Math.random() * 30)
+let tok = Math.floor(Math.random() * 10)
+let hadesb = Math.floor(Math.random() * 4000)
+let expp = Math.floor(Math.random() * 5000)
 
-handler.limit = 1;
+  global.db.data.users[m.sender].limit += dia
+  global.db.data.users[m.sender].money += hadesb
+  global.db.data.users[m.sender].joincount += tok
+  global.db.data.users[m.sender].exp += expp
+  
+let texto = `🐧 𝗣𝗶𝗻𝗴𝘂𝗶 𝗗𝗼𝗻𝗮𝗿 🐧\n\n𝗘𝘀𝗲 𝗘𝘀 𝗘𝗹 𝗬𝗮𝗽𝗲 𝗘𝗻 𝗤𝗿 𝗣𝗼𝗿 𝗦𝗶 𝗗𝗲𝘀𝗲𝗮𝘀 𝗔𝗽𝗼𝘆𝗮𝗿 , 𝗖𝗼𝗺𝗽𝗿𝗮𝗿 𝗔𝗹𝗴𝘂𝗻 𝗦𝗲𝗿𝘃𝗲𝗿 , 𝗖𝗼𝗺𝗽𝗿𝗮𝗿 𝗕𝗼𝘁 𝗢 𝗧𝗯𝗺 𝗦𝗶 𝗗𝗲𝘀𝗲𝗮𝘀 𝗘𝗹 𝗡𝘂𝗺𝗲𝗿𝗼 𝗗𝗲𝗹 𝗬𝗮𝗽𝗲 𝗘𝘀 : 𝟵𝟯𝟲 𝟵𝟵𝟰 𝟭𝟱𝟱 💸`
+
+const fkontak = {
+	"key": {
+    "participants":"0@s.whatsapp.net",
+		"remoteJid": "status@broadcast",
+		"fromMe": false,
+		"id": "Halo"
+	},
+	"message": {
+		"contactMessage": {
+			"vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
+		}
+	},
+	"participant": "0@s.whatsapp.net"
+}
+await conn.sendFile(m.chat, img, 'hades.jpg', texto, fkontak)
+//await conn.sendButton(m.chat, texto, wm, img, [['🔰 𝙼𝙴𝙽𝚄', '/menu'] ], fkontak, m)  
+global.db.data.users[m.sender].lastcofre = new Date * 1
+}
+handler.help = ['daily']
+handler.tags = ['xp']
+handler.command = ['purgatorio'] 
+export default handler
+
+function pickRandom(list) {
+return list[Math.floor(Math.random() * list.length)]}
+
+function msToTime(duration) {
+  var milliseconds = parseInt((duration % 1000) / 100),
+    seconds = Math.floor((duration / 1000) % 60),
+    minutes = Math.floor((duration / (1000 * 60)) % 60),
+    hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
+
+  hours = (hours < 10) ? "0" + hours : hours
+  minutes = (minutes < 10) ? "0" + minutes : minutes
+  seconds = (seconds < 10) ? "0" + seconds : seconds
+
+  return hours + " Horas " + minutes + " Minutos"
+}
