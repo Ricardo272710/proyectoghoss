e044690c5f63c66a752c3614a3e1d93f686f2aff
YallicoPe <yhairmusica950@gmail.com>
2024-06-30 23:55:34 -0500

    Add files via upload

--- plugins/config-on y off.js
+++ plugins/config-on y off.js
@@ -396,17 +396,6 @@ throw false
 chat.simi = isEnable
 break
 
-case 'modoia':
-case 'chatgpt':
-case 'ia':
-isAll = true;
-if (!isOwner) {
-global.dfail('owner', m, conn);
-throw false;
-}
-bot.modoia = isEnable;      
-break;      
-      
 case 'swonly': case 'statusonly':
 isAll = true
 if (!isROwner) {
@@ -416,16 +405,7 @@ throw false
 global.opts['swonly'] = isEnable
 break
 default:
-if (!/[01]/.test(command)) return await conn.reply(m.chat, `${lenguajeGB.smsConfi10()}\n\n🌟 ${lenguajeGB.smsConfi2()} *@${toUser}*
-
-${lenguajeGB.smsConfi3()}
-${lenguajeGB.smsConfi4()}
-
-${lenguajeGB.smsConfi5()}
-${lenguajeGB.smsConfi6()}
-${lenguajeGB.smsConfi7()}
-${lenguajeGB.smsConfi8()}
-${m.isGroup ? `` : `${lenguajeGB.smsConfi9()}`}
+if (!/[01]/.test(command)) return await conn.reply(m.chat, `*OPCIONES PARA ACTIVA Y DESACTIVAR*
 
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
 
@@ -602,13 +582,7 @@ ${wm}`, fkontak, { mentions: [aa,] })
 //conn.sendMessage(m.chat, texto, {quoted: fkontak})	
 throw false
 }
-await conn.reply(m.chat, `${lenguajeGB['smsAvisoRG']()} *_${lenguajeGB['smsMens1']()}_* *|* ${type} 
- *_${lenguajeGB['smsMens2']()}_* *|* ${isEnable ? lenguajeGB.smsEncender() : lenguajeGB.smsApagar()} 
- *_${lenguajeGB['smsMens3']()}_* *|* ${isAll ? lenguajeGB.smsMens4() : isUser ? '' : lenguajeGB.smsMens5()}`, fkontak, m)}	
-/*await conn.sendButton(m.chat, `${lenguajeGB['smsAvisoRG']()}ღ *_${lenguajeGB['smsMens1']()}_* *|* ${type} 
-ღ *_${lenguajeGB['smsMens2']()}_* *|* ${isEnable ? lenguajeGB.smsEncender() : lenguajeGB.smsApagar()} 
-ღ *_${lenguajeGB['smsMens3']()}_* *|* ${isAll ? lenguajeGB.smsMens4() : isUser ? '' : lenguajeGB.smsMens5()}`, wm, null, [[`${isEnable ? lenguajeGB.smsApagar() : lenguajeGB.smsEncender()}`, `${isEnable ? `.off ${type}` : `.on ${type}`}`], [lenguajeGB.smsConMenu(), '.menu']], fkontak, m)}*/
-
+await conn.reply(m.chat, `*${isEnable ? '🐉' : '🐉'} La función ${type} se a ${isEnable ? 'activado' : 'desactivado'} en ${isAll ? 'este bot' : isUser ? '' : 'este chat'}*`, fkontak, m)}
 handler.help = ['en', 'dis'].map(v => v + 'able <option>')
 handler.tags = ['group', 'owner']
 handler.command = /^((en|dis)able|(tru|fals)e|(turn)?o(n|ff)|[01])$/i
