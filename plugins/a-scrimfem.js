f32d9047e26fcc352607c790eb4c2de6a8ed3122
Ricardo272710 <168157929+Ricardo272710@users.noreply.github.com>
2024-07-06 19:59:58 -0500

    Create a-scrimfem.js

---
+++ plugins/a-scrimfem.js
@@ -0,0 +1,32 @@
+const handler = async (m, {conn}) => {
+  m.reply(global.scrimfem);
+};
+handler.command = /^(scrimfem|scrimfem)$/i;
+export default handler;
+handler.admin = true
+handler.group = true
+handler.botAdmin = true
+
+global.scrimfem = `
+* 𝐒𝐂𝐑𝐈𝐌 𝐅𝐄𝐌𝐄𝐍𝐈𝐍𝐎 •
+             
+🕦 𝐇𝐎𝐑𝐀:
+
+      𝐉𝐔𝐆𝐀𝐃𝐎𝐑𝐀𝐒:
+🍄 ┇ 
+🍄 ┇  
+🍄 ┇ 
+🍄 ┇  
+
+      𝐒𝐔𝐏𝐋𝐄𝐍𝐓𝐄𝐒:
+🍄 ┇ 
+🍄 ┇
+
+      𝐈𝐍𝐅𝐎𝐑𝐌𝐀𝐂𝐈𝐎́𝐍:
+
+📍┇ 𝐂𝐀𝐒𝐈𝐋𝐋𝐀:
+👕┇ 𝐕𝐄𝐒𝐓𝐈𝐌𝐄𝐍𝐓𝐀:
+🎆┇ 𝐌𝐀𝐏𝐀𝐒:
+
+ 📑┇𝐋𝐄𝐄𝐑 𝐑𝐄𝐆𝐋𝐀𝐒❕
+`;
