9daef3c61cc8c8b806df77533baf9b9aeab3d96d
Ricardo272710 <168157929+Ricardo272710@users.noreply.github.com>
2024-07-06 22:19:11 -0500

    Create a-vs8fem.js

---
+++ plugins/a-vs8fem.js
@@ -0,0 +1,33 @@
+const handler = async (m, {conn}) => {
+  m.reply(global.vs8fem);
+};
+handler.command = /^(vs8fem|vs8fem)$/i;
+export default handler;
+handler.admin = true
+handler.group = true
+handler.botAdmin = true
+
+global.vs8fem = `
+* 𝐌𝐀𝐏𝐀 𝐆𝐑𝐀𝐍𝐃𝐄 𝐅𝐄𝐌 •
+                𝟖𝐕𝐒𝟖
+
+🕦 𝐇𝐎𝐑𝐀:
+
+      𝐄𝐒𝐂𝐔𝐀𝐃𝐑𝐀 𝟏:
+🍃 ┇ 
+🍂 ┇  
+🍂 ┇ 
+🍂 ┇  
+
+      𝐄𝐒𝐂𝐔𝐀𝐃𝐑𝐀 𝟐:
+ 🍂┇ 
+ 🍃┇
+ 🍃┇ 
+ 🍃┇
+
+      𝐈𝐍𝐅𝐎𝐑𝐌𝐀𝐂𝐈𝐎́𝐍:
+
+📍 ┇ 𝐂𝐀𝐒𝐈𝐋𝐋𝐀𝐒:
+👕 ┇ 𝐕𝐄𝐒𝐓𝐈𝐌𝐄𝐍𝐓𝐀:
+ 📑┇ 𝐋𝐄𝐄𝐑 𝐑𝐄𝐆𝐋𝐀𝐒❕
+`;
