10f27bc1be470f7e1b22a9192b740bd87a6f59d0
Ricardo272710 <168157929+Ricardo272710@users.noreply.github.com>
2024-07-15 23:23:50 -0500

    Update gc-hidetag.js

--- plugins/gc-hidetag.js
+++ plugins/gc-hidetag.js
@@ -2,7 +2,7 @@ let generateWAMessageFromContent = (await import(global.baileys)).default
 import * as fs from 'fs'
 
 var handler = async (m, { conn, text, participants, isOwner, isAdmin }) => {
-if (!m.quoted && !text) return m.reply(`${lenguajeGB['smsAvisoMG']()} 𝙔 𝙀𝙇 𝙏𝙀𝙓𝙏𝙊?`) 
+if (!m.quoted && !text) return m.reply(${lenguajeGB['smsAvisoMG']()} 𝙔 𝙀𝙇 𝙏𝙀𝙓𝙏𝙊?) 
 try { 
 let users = participants.map(u => conn.decodeJid(u.id))
 let q = m.quoted ? m.quoted : m || m.text || m.sender
@@ -19,7 +19,7 @@ let mime = (quoted.msg || quoted).mimetype || ''
 let isMedia = /image|video|sticker|audio/.test(mime)
 let more = String.fromCharCode(8206)
 let masss = more.repeat(850)
-let htextos = `${text ? text : "*Hola!!*"}`
+let htextos = ${text ? text : "*Hola!!*"}
 if ((isMedia && quoted.mtype === 'imageMessage') && htextos) {
 var mediax = await quoted.download?.()
 conn.sendMessage(m.chat, { image: mediax, mentions: users, caption: htextos, mentions: users }, { quoted: m })
@@ -28,15 +28,15 @@ var mediax = await quoted.download?.()
 conn.sendMessage(m.chat, { video: mediax, mentions: users, mimetype: 'video/mp4', caption: htextos }, { quoted: m })
 } else if ((isMedia && quoted.mtype === 'audioMessage') && htextos) {
 var mediax = await quoted.download?.()
-conn.sendMessage(m.chat, { audio: mediax, mentions: users, mimetype: 'audio/mp4', fileName: `Hidetag.mp3` }, { quoted: m })
+conn.sendMessage(m.chat, { audio: mediax, mentions: users, mimetype: 'audio/mp4', fileName: Hidetag.mp3 }, { quoted: m })
 } else if ((isMedia && quoted.mtype === 'stickerMessage') && htextos) {
 var mediax = await quoted.download?.()
 conn.sendMessage(m.chat, {sticker: mediax, mentions: users}, { quoted: m })
 } else {
 await conn.sendMessage(m.chat, { text : text ? text : '' , mentions: users}, { quoted: null, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})
-//await conn.relayMessage(m.chat, {extendedTextMessage:{text: `${masss}\n${htextos}\n`, ...{ contextInfo: { mentionedJid: users, externalAdReply: { thumbnail: imagen1, sourceUrl: md }}}}}, {})
+//await conn.relayMessage(m.chat, {extendedTextMessage:{text: ${masss}\n${htextos}\n, ...{ contextInfo: { mentionedJid: users, externalAdReply: { thumbnail: imagen1, sourceUrl: md }}}}}, {})
 }}}
-handler.command = /^(hidetag|noti|aviso|notificar|n|notify)$/i
+handler.command = /^(hidetag|n|aviso|noti|notificar|notify)$/i
 handler.group = true
 handler.admin = true
 export default handler
