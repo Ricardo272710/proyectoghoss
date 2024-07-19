fe1612d6da84dced11ff048827e3aba415180c4a
Ricardo272710 <168157929+Ricardo272710@users.noreply.github.com>
2024-07-18 22:59:02 -0500

    Update fun-follar.js

--- plugins/fun-follar.js
+++ plugins/fun-follar.js
@@ -1,17 +1,14 @@
-const handler = async (m, {conn, usedPrefix, command, text}) => {
-  let who;
-  if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : false;
-  else who = text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.chat;
-  const user = global.db.data.users[who];
-  if (!who) return m.reply(`*✳️ menciona a quien deseas follar*\n\n*📌 ejemplo :*\n${usedPrefix + command} @tag`);
-
-
-  const abrazo = await conn.reply(m.chat, `@${m.sender.split('@')[0]}  se esta follando duro a @${who.split('@')[0]} `, m, {mentions: [who, m.sender]});
-
-  conn.sendMessage(m.chat, {react: {text: '👌🏼', key: abrazo.key}});
+let handler = async (m, {conn, text}) => {
+let coger = `🤤👅🥵 𝐀𝐂𝐀𝐁𝐀𝐒 𝐃𝐄 𝐅𝐎𝐋𝐋𝐀𝐑𝐓𝐄𝐋@!🥵👅🤤
+    
+  𝙏𝙚 𝙖𝙘𝙖𝙗𝙖𝙨 𝙙𝙚 𝙛𝙤𝙡𝙡𝙖𝙧 𝙖 𝙡𝙖 𝙥𝙚𝙧𝙧𝙖 𝙙𝙚 ${text} ⁩ 𝙖 𝟰 𝙥𝙖𝙩𝙖𝙨 𝙢𝙞𝙚𝙣𝙩𝙧𝙖𝙨 𝙩𝙚 𝙜𝙚𝙢𝙞𝙖 𝙘𝙤𝙢𝙤 𝙪𝙣𝙖 𝙢𝙖𝙡𝙙𝙞𝙩𝙖 𝙥𝙚𝙧𝙧𝙖 "𝐀𝐚𝐚𝐡.., 𝐀𝐚𝐚𝐡𝐡, 𝐬𝐢𝐠𝐮𝐞, 𝐧𝐨 𝐩𝐚𝐫𝐞𝐬, 𝐧𝐨 𝐩𝐚𝐫𝐞𝐬.." 𝙮 𝙡𝙖 𝙝𝙖𝙨 𝙙𝙚𝙟𝙖𝙙𝙤 𝙩𝙖𝙣 𝙧𝙚𝙫𝙚𝙣𝙩𝙖𝙙𝙖 𝙦𝙪𝙚 𝙣𝙤 𝙥𝙪𝙚𝙙𝙚 𝙨𝙤𝙨𝙩𝙚𝙣𝙚𝙧 𝙣𝙞 𝙨𝙪 𝙥𝙧𝙤𝙥𝙞𝙤 𝙘𝙪𝙚𝙧𝙥𝙤 𝙡𝙖 𝙢𝙖𝙡𝙙𝙞𝙩𝙖 𝙯𝙤𝙧𝙧𝙖!
+    
+  ${text} 
+🤤🥵 ¡𝐘𝐀 𝐓𝐄 𝐇𝐀𝐍 𝐅𝐎𝐋𝐋𝐀𝐃𝐎! 🥵🤤
+ `.trim();
+  m.reply(coger, null, {mentions: conn.parseMention(coger)});
 };
-handler.help = ['follar @user'];
-handler.tags = ['fun'];
-handler.command = ['follar'];
-handler.group = true;
+handler.help = ["follar"];
+handler.tags = ["fun"];
+handler.command = /^(coger|violar|follar)$/i;
 export default handler;
