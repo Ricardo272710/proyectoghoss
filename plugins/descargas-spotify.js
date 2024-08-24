fffe80b386f32e97b49acca6160de47b641aa247
Ricardo272710 <168157929+Ricardo272710@users.noreply.github.com>
2024-08-24 02:24:27 -0500

    Update descargas-spotify.js

--- plugins/descargas-spotify.js
+++ plugins/descargas-spotify.js
@@ -1,114 +1,41 @@
-import axios from 'axios'
-import fetch from 'node-fetch'
-import { youtubedl, youtubedlv2 } from '@bochilteam/scraper'
-import search from 'yt-search'
-async function spotifyxv(query) {
-let token = await tokens();
-let response = await axios({
-method: 'get',
-url: 'https://api.spotify.com/v1/search?q=' + encodeURIComponent(query) + '&type=track',
-headers: {
-Authorization: 'Bearer ' + token,
-},
-})
-const tracks = response.data.tracks.items
-const results = tracks.map((track) => ({
-name: track.name,
-artista: track.artists.map((artist) => artist.name),
-album: track.album.name,
-duracion: timestamp(track.duration_ms),
-url: track.external_urls.spotify,
-imagen: track.album.images.length ? track.album.images[0].url : '',
-}))
-return results
-}
-async function tokens() {
-const response = await axios({
-method: 'post',
-url:
-'https://accounts.spotify.com/api/token',
-headers: {
-'Content-Type': 'application/x-www-form-urlencoded',
-Authorization: 'Basic ' + Buffer.from('acc6302297e040aeb6e4ac1fbdfd62c3:0e8439a1280a43aba9a5bc0a16f3f009').toString('base64'),
-},
-data: 'grant_type=client_credentials',
-})
-return response.data.access_token
-}
-function timestamp(time) {
-const minutes = Math.floor(time / 60000);
-const seconds = Math.floor((time % 60000) / 1000);
-return minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
-}
-async function getBuffer(url, options) {
-try {
-options = options || {};
-const res = await axios({
-method: 'get',
-url,
-headers: {
-DNT: 1,
-'Upgrade-Insecure-Request': 1,
-},
-...options,
-responseType: 'arraybuffer',
-});
-return res.data;
-} catch (err) {
-return err;
-}}
-async function getTinyURL(text) {
-try {
-let response = await axios.get(`https://tinyurl.com/api-create.php?url=${text}`);
-return response.data;
-} catch (error) {
-return text;
-}}
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) throw `╰⊱❗️⊱ *ACCIÓN MAL USADA* ⊱❗️⊱╮\n\n🍟 *DEBE DE USAR EL COMANDO COMO EN ESTE EJEMPLO:*\n${usedPrefix + command} *tu foto*`
-try {
-conn.reply(m.chat, '🚩 *Enviando su música de Spotify*', m, {
-contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
-title: packname,
-body: wm,
-previewType: 0, thumbnail: icons,
-sourceUrl: channel }}})
-m.react(rwait)
-let songInfo = await spotifyxv(text)
-if (!songInfo.length) throw `*No se encontró la canción*`
-let res = songInfo[0]
-let fileSizeInMB = (await getBuffer(res.url)).length / (1024 * 1024)
-let shortURL = await getTinyURL(res.url)
-const info = `🍟 *TITULO:*
-_${res.name}_
+// TheMystic-Bot-MD@BrunoSobrino - descargas-spotify.js
+// Creditos de los tags a @darlyn1234 y diseño a @ALBERTO9883
+import fetch from 'node-fetch';
+import fs from 'fs';
+import axios from 'axios';
 
-🚩 *ARTISTA:*
-» ${res.artista.join(', ')}
+const handler = async (m, { conn, text, usedPrefix, command }) => {
+ if (!text) return await conn.reply(m.chat, `¿Que esta buscando? ingresa el nombre para descargar sus música de Spotify, Ejemplo:* ${usedPrefix + command} ozuna`, m, {contextInfo: {externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: mg, body: wm, previewType: 0, thumbnail: img.getRandom(), sourceUrl: redes.getRandom()}}})    
+m.react(`⌛`) 
+  try {
+    const res = await fetch(global.API('CFROSAPI', '/api/spotifysearch?text=' + text))
+    const data = await res.json()
+    const linkDL = data.spty.resultado[0].link;
+    const musics = await fetch(global.API('CFROSAPI', '/api/spotifydl?text=' + linkDL))
+    const music = await conn.getFile(musics.url)
+    const infos = await fetch(global.API('CFROSAPI', '/api/spotifyinfo?text=' + linkDL))
+    const info = await infos.json()
+    const spty = info.spty.resultado
+    const img = await (await fetch(`${spty.thumbnail}`)).buffer()  
 
-🔗 *LINK:*
-» ${shortURL}
+let spotifyi = `*• Titulo:* ${spty.title}
+*• Artista:* ${spty.artist}
+*• Álbum:* ${spty.album}
+*• Publicado:* ${spty.year}
 
-✨️ *Enviando Canción....*
-${global.wm}`
-
-let resImg = await fetch(res.imagen)
-let thumbb = await resImg.buffer()
-let { videos } = await search(res.name)
-let q = '128kbps'
-let v = videos[0].url
-let yt = await youtubedl(v).catch(async (_) => await youtubedlv2(v))
-let dl_url = await yt.audio[q].download()
-let ttl = await yt.title
-let size = await yt.audio[q].fileSizeH
-let img = await getBuffer(res.imagen)
-conn.sendMessage(m.chat, { audio: { url: dl_url }, fileName: `${ttl}.mp3`, mimetype: 'audio/mpeg' }, { quoted: m })
-await conn.sendMessage(m.chat, {text: info, contextInfo: {forwardingScore: 9999999, isForwarded: true, "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.wm, "containsAutoReply": true, "mediaType": 1, "thumbnail": img, "thumbnailUrl": img, "mediaUrl": shortURL, "sourceUrl": shortURL}}}, {quoted: fkontak});
-m.react(done)
-} catch (error) {
-}}
-handler.tags = ['descargas']
+> 🚀 *ᴱⁿᵛᶦᵃⁿᵈᵒ ᶜᵃⁿᶜᶦᵒ́ⁿ ᵃᵍᵘᵃʳᵈᵉ ᵘⁿ ᵐᵒᵐᵉⁿᵗᵒ....*`
+await conn.reply(m.chat, spotifyi, fkontak, {contextInfo: {externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: wm, body: '', previewType: 0, thumbnail: spty.data.cover_url, sourceUrl: redes.getRandom()}}}) 
+await conn.sendMessage(m.chat, {audio: music.data, fileName: `${spty.name}.mp3`, mimetype: 'audio/mpeg'}, {quoted: m})
+m.react(`✅`) 
+handler.limit = 1
+  } catch (error) {
+    console.error(error);
+m.react(`❌`) 
+  }};
 handler.help = ['spotify']
-handler.group = true;
+handler.tags = ['downloader']
+handler.command = /^(spotify|music)$/i
 handler.register = true
-handler.command = ['spotify','music']
+//handler.limit = 1
+handler.level = 2
 export default handler
